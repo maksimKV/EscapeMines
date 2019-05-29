7fb60ddad035a4c3a8f619e95de621b4bf40af6a
Maksim Kanev <maks_kv@yahoo.com>
2019-05-29 16:36:54 +0300

    Add project files.

---
+++ GameSimulation/GameSimulation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSimulation
+{
+    public class GameSimulation
+    {
+        private int numRows;
+        private int numColumns;
+        private int[,] mines;
+        //private Dictionary<int, int> mines = new Dictionary<int, int>();
+    
+        public GameSimulation(int rows, int columns)
+        {
+            numRows = rows;
+            numColumns = columns;
+        }
+
+        private void CreateField()
+        {
+
+        }
+
+        private void GenerateMines()
+        {
+            int numMines = (numRows + numColumns) / 5;
+            Random random = new Random();
+
+            do
+            {
+                int x = random.Next(0, numRows);
+                int y = random.Next(0, numColumns);
+
+                if(!(mines.ContainsKey(x) && mines[x].Equals(y)))
+                {
+                    mines.Add(x, y);
+
+                    numMines--;
+                }
+
+            } while (numMines > 0);
+        }
+    }
+}
